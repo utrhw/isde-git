b0d1eb06f67c8c8d0e3513b104dc8dad7f37d52f
bat <battista.biggio@unica.it>
2020-10-26 12:54:14 +0100

    Initial commit

--- src/classifiers/nmc.py
+++ src/classifiers/nmc.py
@@ -30,28 +30,12 @@ class NMC(object):
     def centroids(self):
         return self._centroids
 
-    # @centroids.setter
-    # def centroids(self, value):
-    #    self._centroids = value
-
     @property
     def class_labels(self):
         return self._class_labels
 
-    def fit(self, Xtr, ytr):
-        self._class_labels = np.unique(ytr)
-        num_classes = self._class_labels.size
-        self._centroids = np.zeros(shape=(num_classes, Xtr.shape[1]))
-        for k in range(num_classes):
-            xk = Xtr[ytr == self._class_labels[k], :]
-            self._centroids[k, :] = np.mean(xk, axis=0)
+    def fit(self, xtr, ytr):
+        pass
 
-    def predict(self, Xts):
-
-        if self._centroids is None:
-            raise ValueError("The classifier is not trained. Call fit first!")
-
-        dist_euclidean = euclidean_distances(Xts, self._centroids)
-        idx_min = np.argmin(dist_euclidean, axis=1)
-        yc = self._class_labels[idx_min]
-        return yc
+    def predict(self, xts):
+        pass
--- src/fun_utils.py
+++ src/fun_utils.py
@@ -26,24 +26,9 @@ def load_data(filename):
     return X, y
 
 
-def split_data(X, y, tr_fraction=0.5):
+def split_data(x, y, tr_fraction=0.5):
     """
-    Split the data X,y into two random subsets
+    Split the data x, y into two random subsets
 
     """
-    num_samples = y.size
-    n_tr = int(num_samples * tr_fraction)
-
-    idx = np.array(range(0, num_samples))
-    np.random.shuffle(idx)  # shuffle the elements of idx
-
-    tr_idx = idx[0:n_tr]
-    ts_idx = idx[n_tr:]
-
-    Xtr = X[tr_idx, :]
-    ytr = y[tr_idx]
-
-    Xts = X[ts_idx, :]
-    yts = y[ts_idx]
-
-    return Xtr, ytr, Xts, yts
+    pass
