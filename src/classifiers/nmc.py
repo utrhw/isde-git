3b72d7c9a6795499b49d1858c93b4b5aff883d98
utrhw <utrhw@student.kit.edu>
2024-10-21 10:16:40 +0200

    added code for fit function of nmc classifier

--- src/classifiers/nmc.py
+++ src/classifiers/nmc.py
@@ -35,7 +35,28 @@ class NMC(object):
         return self._class_labels
 
     def fit(self, xtr, ytr):
-        pass
+        """_summary_
+
+        Args:
+            xtr (_type_): _description_
+            ytr (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+
+        n_dimensions = xtr.shape[1]
+        n_classes = np.unique(ytr).size
+
+        self._centroids = np.zeros((n_classes, n_dimensions))
+
+        for i in range(0,n_classes):
+            self._centroids[i,:]=np.mean(xtr[ytr==i,:],axis=0) #axis = dimension to remove
+            #plt.subplot(2,5,i+1)
+            #plt.imshow(centroids[i,:].reshape((28,28)))
+
+        return self
+
 
     def predict(self, xts):
         pass
